a449d9213cb941212d4b9ad81650f3a5751fd1e2
mokone-september <mokoneseptember@gmail.com>
2025-04-15 18:19:56 +0200

    feeature : updated - app.tsx - app.styled.ts - main.tsx - react-dark-mode-toggle.d.ts - theme.ts

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,7 +15,7 @@ const Header: React.FC = () => {
       <Title>React Weather</Title>
       <HeaderIconsContainer>
         <DarkModeToggle checked={isDarkMode} onChange={() => dispatch(toggleDarkMode())} size={60} />
-        <GithubLink href="http://www.github.com/mokone-september/weather-app">
+        <GithubLink href="http://www.github.com/esnz/reactweather">
           <GithubIcon />
         </GithubLink>
       </HeaderIconsContainer>
